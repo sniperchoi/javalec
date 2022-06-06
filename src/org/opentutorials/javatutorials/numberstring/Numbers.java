19d3e815ed8de6eb8adacf8ed6633d849f2aa59a
sniperchoi <chltmdrnr@gmail.com>
2022-06-06 22:21:20 +0900

    2345

--- src/org/opentutorials/javatutorials/numberstring/Numbers.java
+++ src/org/opentutorials/javatutorials/numberstring/Numbers.java
@@ -3,7 +3,7 @@ package org.opentutorials.javatutorials.numberstring;
 public class Numbers {
 
 	public static void main(String[] args) {
-		System.out.println(1+2);
+		System.out.println(2+2);
 
 	}
 
